f9e2bf13712a6d5f4d1888c01ca633d6e429b10c
ssilwa <silwal@mit.edu>
2017-08-22 16:52:38 -0400

    Updated Readme

--- README.md
+++ README.md
@@ -1,14 +1,40 @@
 ### Introduction
-### Features:
+
+This is the updated midnight maker script for ZBT Fraternity at MIT.
+
 
 ### Installation
 
-> **Note:** just a note
+See the directions here: https://help.github.com/articles/cloning-a-repository/
+
+### How to Use
 
 
-# Python:
-```Python
-while True:
-    print('Hello')
-```
+
+### Overview of the Algorithm
+
+There are three main parts to the midnight assigning algorithm.
+
+# Specification
+
+Every brother gets to specify two things:
+
+1) The tasks they would like to do
+2) The days they would like to work
+
+# Bucketing
+
+Brothers are bucketed into four quartiles based on their current midnight points.
+Quartle 1 has the most points and quartile 4 has the least points.
+Since quartile 1 bros have a lot of points, they should not be doing a lot of midnights
+and similarly, quartile 4 bros should be doing most of the midnights. Currently, the number of tasks per week is 54. The breakdown of tasks allocated to each quartile is:
+
+quartile 1: 7
+quartile 2: 10
+quartile 3: 16
+quartile 4: 21
+
+(roughly 10%, 20%, 30%, 40%).
+
+
 
--- constants.py
+++ constants.py
@@ -1,6 +1,7 @@
 ''' Constants that can be configured.
 Basically the only file that you should change.
 '''
+import math
 
 ###################### Editable Start ######################
 
@@ -17,18 +18,25 @@ tasks_dict = {'Bathrooms1': [0,1,2,3,4,5], 'Bathrooms2': [1,4], \
           'Kitchens1': [0,1,2,3,4,5], 'Kitchens2': [0,1,2,3,4,5], \
           'Waitings1': [0,1,2,3,4], 'Waitings2': [0,1,2,3,4]}
 
-################ Task allocation among quartiles ################
-
-# The total number of tasks is 54 currently
-# Allocation is roughly 40%, 30%, 20%, 10%
-
-quartile_1_tasks = 6
-quartile_2_tasks = 11
-quartile_3_tasks = 16
-quartile_4_tasks = 21
+# Percentage of midnights given to the quartiles
+quartile_2_perct = .2
+quartile_3_perct = .3
+quartile_4_perct = .4
 
 ###################### Editable End ######################
 
 all_tasks = [task + str(day) for task, days in tasks_dict.items() for day in days]
 num_tasks = len(all_tasks)
 days_map = {'0': 'Sunday', '1': 'Monday', '2': 'Tuesday', '3': 'Wednesday', '4': 'Thursday', '5': 'Friday', '6': 'Saturday'}
+
+################ Task allocation among quartiles ################
+
+# The total number of tasks is 54 currently
+# Allocation is roughly 40%, 30%, 20%, 10%
+
+quartile_4_tasks = math.floor(num_tasks*quartile_4_perct)
+quartile_3_tasks = math.floor(num_tasks*quartile_3_perct)
+quartile_2_tasks = math.floor(num_tasks*quartile_2_perct)
+quartile_1_tasks = max(0, num_tasks - quartile_2_tasks - quartile_3_tasks - quartile_4_tasks)
+
+
